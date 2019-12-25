82718042b898408cdcfe1449c6d9bb762849c1b0
Mike Zamansky <zamansky@gmail.com>
2019-12-24 20:52:00 -0500

    started to try a query

--- src/app/renderer/api.cljs
+++ src/app/renderer/api.cljs
@@ -1,18 +1,28 @@
-query { 
-       user(login:"zamansky"){
-                              organizations(first:99){
-                                                      
-      	                                              edges{
-                                                            cursor
-                                                            
-                                                            node{
-                                                                 
-                                                                 name 
-                                                                 }
-                                                            }
-                                                      
-                                                      }
-                              
-                              }
+(ns app.renderer.api
+  (:require
+   [graphql-query.core :refer [graphql-query]]
+   ))
 
-       }
+(graphql-query  {:queries [[:user {:login "zamansky"}
+                            [:organizations {:first 99}
+                             [:edges  [cursor :node [:name]]]]]]})
+
+
+;; query { 
+;;        user(login:"zamansky"){
+;;                               organizations(first:99){
+
+;;       	                                              edges{
+;;                                                             cursor
+
+;;                                                             node{
+
+;;                                                                  name 
+;;                                                                  }
+;;                                                             }
+
+;;                                                       }
+
+;;                               }
+
+;;        }

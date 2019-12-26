deefd3e1dbc08ae1ace13a35788817a58f96a867
Mike Zamansky <zamansky@gmail.com>
2019-12-26 11:04:32 -0500

    starting to get the repos - need a go-loop

--- src/app/renderer/api.cljs
+++ src/app/renderer/api.cljs
@@ -6,13 +6,28 @@
    ))
 
 (defn load-orgs-into-state [state]
-  (prn state)
-  (go (let [response (<! (http/get "https://api.github.com/user/orgs" {:with-credentials? false
-                                                                       :headers {"Authorization" (str "Basic " (get @state  :credentials))}
-                                                                       }))]
-
+  (go (let [response
+            (<! (http/get "https://api.github.com/user/orgs" {:with-credentials? false
+                                                              :headers {"Authorization" (str "Basic " (get @state  :credentials))}
+                                                              }))]
         (let [orglist (mapv #(:login %) (:body response))]
           (swap! state assoc :orgs orglist :org (first orglist) )
           ))))
 
+(defn load-all-repos [state]
+  (let [org (:org @state)
+        creds (:credentials @state)
+        url (goog.string/format "https://api.github.com/orgs/%s/repos" org)
+        ]
+    (go
+      (let [response (<! (http/get url {:with-credentials? false
+                                        :headers {"Authorization"
+                                                  (goog.string/format "Basic %s" creds)}}))]
+        (let [l (get (:headers response) "link")]
+          (prn "THE LINK: " l)
+          )
+        )
+      )
+    )
+  )
 

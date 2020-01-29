3b6a9d76dd836b7001acca6e0b7aa452c18aeaaf
Mike Zamansky <zamansky@gmail.com>
2020-01-29 13:02:33 -0500

    converting main to do oauth - up to init the windows

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -1,8 +1,50 @@
 (ns app.main.core
-  (:require ["electron" :refer [app BrowserWindow crashReporter]]))
+  (:require ["electron" :refer [app BrowserWindow crashReporter ipcMain ]]
+            [app.main.keys :as k]
+            [cljs.core.async :refer (chan put! <! >! go go-loop timeout)]
+            [cljs-http.client :as http]
+            ))
+
+
 
 (def main-window (atom nil))
-;; start to convert this to oauth
+(def auth-window (atom nil))
+(def token (atom ""))
+
+;;------------------------------------------
+(defn extract-code [url]
+  (second  (re-matches #".*code=([0-9a-f]+).*" url))
+  )
+
+(defn extract-token [url]
+  (second  (re-matches #".*token=([0-9a-f]+).*" url)))
+
+
+(.on ipcMain "get-token"
+     #(.reply %1 "token" @token)
+     )
+
+(defn get-token [url]
+  (let  [code (extract-code url)
+         url "https://github.com/login/oauth/access_token"
+         ]
+    (go (let [response
+              (<! (http/get url {:query-params {"client_id" k/client_id "client_secret" k/client_secret "code" code }
+                                 :headers {"Accept" "application/json"}
+                                 }))
+              ]
+          (reset! token (extract-token (:body response)))
+          #_(prn @token)
+          (.loadURL @auth-window (str "file://" js/__dirname "/public/index.html"))
+          
+          ))))
+
+
+
+
+;;-------------------------------------------
+;; up to init browser
+
 (defn init-browser []
   (reset! main-window (BrowserWindow.
                        (clj->js {:width 1024

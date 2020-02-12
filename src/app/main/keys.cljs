ce9e2c6bd08a395378b9ba97c26df41af76df439
Mike Zamansky <zamansky@gmail.com>
2020-02-12 15:29:02 -0500

    added notes file

--- src/app/main/keys.cljs
+++ src/app/main/keys.cljs
@@ -1,7 +1,7 @@
 (ns app.main.keys)
 
-(def client_id "")
-(def client_secret "")
-(def scope "repo admin:org delete_repo")
+(def client_id "3056f749c2e91ce4d780")
+(def client_secret "8dbd436a98a63894a7408f811ea764ce8a6ad0ff")
+(def scope "repo read:org user admin:org delete_repo")
 (def auth-url-base "https://github.com/login/oauth/authorize?client_id=")
 ( def auth-url (str auth-url-base client_id "&scope=" scope))
